f666a0ad61b59e721008e4e5e12b2390a5e06ad1
Rocodes <Rksingh620@gmail.com>
2020-04-24 09:24:14 +0530

    Updates

---
+++ precache-manifest.2c7aade26b82d99a7e8640572a215dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c18106023c5b9dcf268985346d9cf08",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "566276f058817070fc71",
+    "url": "/covid-19/static/css/main.185fe450.chunk.css"
+  },
+  {
+    "revision": "4ebe7291afceead46f90",
+    "url": "/covid-19/static/js/2.ff09b224.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.ff09b224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566276f058817070fc71",
+    "url": "/covid-19/static/js/main.1f314cc8.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  }
+]);
